7a6dd9da0a1903bf3e490f3c2dfc86861c1cc277
Cassie Schultz <schu4276@morris.umn.edu>
2020-08-28 12:07:50 -0500

    I have made some additional comments for clarity as per feedback from nic I also added to the script so that the scratch directory is deleted at the end of the script

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -2,6 +2,8 @@
 # second script of lab 0
 ##### CONSTANTS
 ARCHIVE_NAME=$1
+# capturing the base name here for later use, this removes the .tgz and 
+# allows be to rename files using only the important part of the name
 BASE=$(basename -s .tgz "$ARCHIVE_NAME")
 #### MAIN
 SCRATCH=$(mktemp -d)
@@ -15,9 +17,16 @@ cd "$SCRATCH" || exit
 # remove all the files that contain "DELETE ME"
 # the -l flag displays the filenames that contain the delete me phrase 
 grep -Rl "DELETE ME" . | xargs rm   
-# Creating a new compressed tar archive that contains the files left over in the scratch di
+# Creating a new compressed tar archive that contains the files left over in the scratch directory
 tar -zcf cleaned_"$ARCHIVE_NAME" "$BASE"
 mv cleaned_"$ARCHIVE_NAME" "$HERE"
+
+
+# deleting the scratch dir
+cd "$SCRATCH" || exit
+rm -rf "$SCRATCH"
+# returning to the directory the user expects to be in
 cd "$HERE" || exit 
-#delete the scratch dir
+
+
 
